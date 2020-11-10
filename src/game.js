504ddcd447927e413334690099b0aeb1488ab549
Muhammad Usman <70571593+usman557@users.noreply.github.com>
2020-11-10 23:13:50 +0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -23,7 +23,7 @@ export default class Game {
 
   rowWin () {
     let win = false
-    for (let r = 0; r < 3; r++) {
+    for (let r = 0; r < 4; r++) {
       const row = this.board[r]
       if (row[0] === null) { continue }
       win = win || (row[0] === row[1] && row[0] === row[2])
@@ -34,7 +34,7 @@ export default class Game {
 
   colWin () {
     let win = false
-    for (let c = 0; c < 3; c++) {
+    for (let c = 0; c < 4; c++) {
       const col = this.board
       if (col[0][c] === null) { continue }
       win = win || (col[0][c] === col[1][c] && col[0][c] === col[2][c])
